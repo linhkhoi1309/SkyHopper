337cd94e69f0aca95dcdcd56a2f3a1521a0b5188
linhkhoi1309 <luongkhoi3456@gmail.com>
2025-03-31 17:23:17 +0700

    Complete basic UXML + USS + Event Handler for MainMenu

--- Assets/Scripts/Misc/GlobalConfig.cs
+++ Assets/Scripts/Misc/GlobalConfig.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public static class GlobalConfig {
     public const string START_BUTTON_NAME = "StartButton";
+    public const string SETTINGS_BUTTON_NAME = "SettingsButton";
     public const int LEVEL_SCENE_BUILD_INDEX = 1;
 }
--- Assets/Scripts/UI/MainMenu/MainMenuEventHandler.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuEventHandler.cs
@@ -9,17 +9,26 @@ public class MainMenuEventHandler : MonoBehaviour
     UIDocument uIDocument;
     Button startButton;
 
+    Button settingsButton;
+
     private void Awake() {
         uIDocument = GetComponent<UIDocument>();
         startButton = uIDocument.rootVisualElement.Q<Button>(GlobalConfig.START_BUTTON_NAME);
+        settingsButton = uIDocument.rootVisualElement.Q<Button>(GlobalConfig.SETTINGS_BUTTON_NAME);
         startButton.RegisterCallback<ClickEvent>(OnStartGameButtonClicked);
+        settingsButton.RegisterCallback<ClickEvent>(OnSettingsButtonClicked);
     }
 
     private void OnDisable() {
         startButton.UnregisterCallback<ClickEvent>(OnStartGameButtonClicked);
+        settingsButton.UnregisterCallback<ClickEvent>(OnSettingsButtonClicked);
     }
 
     void OnStartGameButtonClicked(ClickEvent click){
         SceneManager.LoadScene(GlobalConfig.LEVEL_SCENE_BUILD_INDEX);
+    }
+
+    void OnSettingsButtonClicked(ClickEvent click){
+        Debug.Log("Settings button clicked");
     }
 }
